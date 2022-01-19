19f5a9350b355bdd91d9489cd3126d5905e0d0d7
BariqDharmawan <dharmawan.muhammad03@gmail.com>
2022-01-19 16:33:10 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const sliderThreeSlideWithNav = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
