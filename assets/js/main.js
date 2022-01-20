419a5abe2ba95fc1d4370d372bba4128232b87ba
BariqDharmawan <dharmawan.muhammad03@gmail.com>
2022-01-21 00:04:28 +0700

    [ADD] detail infografis page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+const sliderOneSlideWithNav = new Swiper(".slider-1", {
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true
+  },
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    clickable: true,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
+
 const sliderThreeSlideWithNav = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -9,4 +26,13 @@ const sliderThreeSlideWithNav = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
+});
+
+const sliderFourSlideWithNav = new Swiper(".slider-4", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
 });
